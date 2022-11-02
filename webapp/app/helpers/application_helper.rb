50953a02c84db01cbf5bd51b44355b7c4862697a
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-11-03 02:00:53 +0330

    implemented: basic logic for the weather function

--- webapp/app/helpers/application_helper.rb
+++ webapp/app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
     def weather(city)
-        
+        api_token = ENV['OPEN_WEATHER_MAP_API']
+        base_url = "https://api.openweathermap.org/data/2.5/weather?city=#{city}&appid=#{api_token}"
     end
 end
