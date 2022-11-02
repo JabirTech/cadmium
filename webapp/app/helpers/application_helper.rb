b8c4cc48678538aead95a916e5a461bcc06bd9eb
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-11-03 02:17:28 +0330

    lat and lon added

--- webapp/app/helpers/application_helper.rb
+++ webapp/app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
-    def weather(city)
+    def weather(lat, lon)
         api_token = ENV['OPEN_WEATHER_MAP_API']
-        base_url = "https://api.openweathermap.org/data/2.5/weather?city=#{city}&appid=#{api_token}"
+        base_url = "https://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{lon}&appid=#{api_token}"
 
         res = HTTParty.get(base_url)
         return res.body
