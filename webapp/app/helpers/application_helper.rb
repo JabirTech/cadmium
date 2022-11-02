969f9ee49d77397ad7fe79998025015d9b5def8d
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-11-03 02:21:08 +0330

    working on the helper stuff

--- webapp/app/helpers/application_helper.rb
+++ webapp/app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
     def weather(lat, lon)
         api_token = ENV['OPEN_WEATHER_MAP_API']
-        base_url = "https://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{lon}&appid=#{api_token}"
+        base_url = "https://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{lon}&units=metric&appid=#{api_token}"
 
         res = HTTParty.get(base_url)
-        return res.body
+        res = res.body 
+
+        return res.class
     end
 end
